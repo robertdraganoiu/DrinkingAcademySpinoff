08df58056aeac08d1b1137d7a58a6cb03e090375
robert <robert.draganoiu@gmail.com>
2024-08-17 16:33:27 +0200

    small updates

--- androidApp/src/main/java/com/hack/drinkingacademy/android/game/RunningGameScreen.kt
+++ androidApp/src/main/java/com/hack/drinkingacademy/android/game/RunningGameScreen.kt
@@ -1,17 +1,19 @@
 package com.hack.drinkingacademy.android.game
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -37,14 +39,7 @@ import java.util.Locale
 @Composable
 fun RunningGameScreen(currentCard: GameCard, onNextChallenge: () -> Unit, difficulty: Int) {
     val tintColor by animateColorAsState(
-        targetValue = when (currentCard.type) {
-            ChallengeType.DARE -> Color.Red.copy(alpha = 0.25f)
-            ChallengeType.TRUTH -> Color.Blue.copy(alpha = 0.25f)
-            ChallengeType.POLL -> Color.Green.copy(alpha = 0.25f)
-            ChallengeType.MASTER -> Color.Yellow.copy(alpha = 0.25f)
-            ChallengeType.TRIVIA -> Color.Magenta.copy(alpha = 0.25f)
-            ChallengeType.BETRAYAL -> Color.Red.copy(alpha = 0.4f)
-        }
+        targetValue = currentCard.type.toColorTint()
     )
 
     Box(
@@ -69,8 +64,6 @@ fun RunningGameScreen(currentCard: GameCard, onNextChallenge: () -> Unit, diffic
         )
 
         Column(
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
@@ -88,36 +81,52 @@ fun RunningGameScreen(currentCard: GameCard, onNextChallenge: () -> Unit, diffic
                     .padding(top = 16.dp)
             )
 
-            // Central Challenge Box with Tropical Theme
-            AnimatedVisibility(visible = true) { // Change visibility for animation
-                Box(
-                    modifier = Modifier
-                        .background(
-                            color = Color(0xFFF0E68C).copy(alpha = 0.85f), // Light khaki color to mimic parchment
-                            shape = RoundedCornerShape(20.dp)
-                        )
-                        .border(
-                            border = BorderStroke(
-                                4.dp,
-                                Color(0xFF8B4513)
-                            ), // Brown border for bamboo feel
-                            shape = RoundedCornerShape(20.dp)
-                        )
-                        .padding(16.dp)
-                        .align(Alignment.CenterHorizontally)
+            Spacer(modifier = Modifier.weight(1f)) // Take up the remaining space
+
+            // Scrollable Challenge Description
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+            ) {
+                LazyColumn(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    contentPadding = PaddingValues(horizontal = 16.dp),
+                    modifier = Modifier.align(Alignment.Center) // Center the LazyColumn
                 ) {
-                    Text(
-                        text = currentCard.description[0],
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Black,
-                        textAlign = TextAlign.Center,
-                        fontFamily = FontFamily(Font(R.font.rubik)),
-                        modifier = Modifier.align(Alignment.Center)
-                    )
+                    items(currentCard.description) { desc ->
+                        ChallengeCard(description = desc)
+                    }
                 }
             }
+
+            Spacer(modifier = Modifier.weight(1f)) // Take up the remaining space
         }
+    }
+}
+
+@Composable
+fun ChallengeCard(description: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .background(
+                color = Color(0xAA000000), // Semi-transparent dark background
+                shape = RoundedCornerShape(10.dp) // Matching the slider's corner radius
+            )
+            .padding(16.dp)
+    ) {
+        Text(
+            text = description,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontFamily = FontFamily(Font(R.font.rubik)),
+            modifier = Modifier.align(Alignment.Center)
+        )
     }
 }
 
@@ -127,4 +136,13 @@ fun Int.toGameBackground() = when (this) {
     3 -> R.drawable.background_difficulty_3
     4 -> R.drawable.background_difficulty_4
     else -> R.drawable.background_difficulty_5
+}
+
+fun ChallengeType.toColorTint() = when (this) {
+    ChallengeType.DARE -> Color.Red.copy(alpha = 0.15f)
+    ChallengeType.TRUTH -> Color.Blue.copy(alpha = 0.15f)
+    ChallengeType.POLL -> Color.Green.copy(alpha = 0.15f)
+    ChallengeType.MASTER -> Color.Yellow.copy(alpha = 0.15f)
+    ChallengeType.TRIVIA -> Color.Magenta.copy(alpha = 0.15f)
+    ChallengeType.BETRAYAL -> Color.Red.copy(alpha = 0.3f)
 }
